998a14c8e8dcbf123b99b9ded6f3bdc7f10e8efa
chenyinheng30 <chenyinheng30@outlook.com>
2022-09-07 09:00:03 +0800

    feat:Complete the Lexer and there's a test Lexer. BREAKING CHANGE:Removed Snippet with its impl. Implement the number of Lisp.

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,7 @@
+
 trait Code {
     fn eat(&mut self) -> Option<char>;
     fn head(&self) -> Option<char>;
-}
-
-struct Lexer<T: Code>{
-    code: T,
-}
-
-impl<T: Code> Lexer<T> {
-    fn new(code: T) -> Self{
-        Lexer{code}
-    }
-
-    fn next(&mut self) -> Snippet{
-        match self.code.head() {
-            Some(c) => match c {
-                ' ' => {
-                    while let Some(' ') = self.code.eat(){}
-                    self.next()
-                }
-
-                sign @ ('(' | ')') => {
-                    self.code.eat();
-                    Snippet::Sign(sign)
-                },
-
-                '"' => {
-                    let mut str = String::new();
-                    while let Some(c) = self.code.eat() {
-                        if c == '"' {
-                            break;
-                        }else{
-                            str.push(c);
-                        }
-                    }
-                    self.code.eat();
-                    Snippet::String(str)
-                }
-
-                ch @ _ => {
-                    let mut str = String::from(ch);
-                    while let Some(c) = self.code.eat() {
-                        match c {
-                            ' ' | ')' | '(' => break,
-                            _ => str.push(c)
-                        }
-                    }
-                    Snippet::Other(str)
-                }
-            }
-
-            None => Snippet::EOF
-        }
-    }
-}
-
-#[derive(Debug)]
-enum Snippet{
-    String(String),
-    Other(String),
-    Sign(char),
-    EOF
-}
-
-fn as_number_char(ch: char) -> Option<f64> {
-    match ch {
-        '0'..='9' => Some(((ch as u8) - ('0' as u8)) as f64),
-        // 'a'..='z' => Some(((ch as u8) - ('a' as u8)) as f64),
-        // 'A'..='Z' => Some(((ch as u8) - ('A' as u8)) as f64),
-        _ => None
-    }
-}
-
-fn as_number_string(str: &String) -> Option<f64> {
-    let mut sum = 0.0;
-    let mut i = 1.0;
-    for x in str.chars().rev() {
-        match as_number_char(x) {
-            Some(x) => sum += x * i,
-            None => return None
-        }
-        i *= 10.0;
-    }
-    Some(sum)
-}
-
-impl Snippet {
-    fn token(self) -> Token {
-        match self{
-            Self::Sign(ch) => Token::Sign(ch),
-            Self::String(str) => Token::String(str),
-            Self::Other(str) => {
-                match as_number_string(&str) {
-                    Some(x) => Token::Number(x),
-                    None => Token::Atom(str)
-                }
-            }
-            Self::EOF => Token::EOF
-        }
-    }
 }
 
 #[derive(Debug)]
@@ -108,6 +11,112 @@ enum Token{
     Atom(String),
     Sign(char),
     EOF,
+}
+
+fn lexer<T>(code: &mut T) -> Token
+    where T: Code
+{
+    match code.head() {
+        Some(c) => match c {
+            ' ' => {
+                while let Some(' ') = code.eat(){}
+                lexer(code)
+            }
+            sign @ ('(' | ')' | '\'' | '`') => {
+                code.eat();
+                Token::Sign(sign)
+            },
+            '"' => {
+                let mut str = String::new();
+                while let Some(c) = code.eat() {
+                    if c == '"' {
+                        break;
+                    }else{
+                        str.push(c);
+                    }
+                }
+                code.eat();
+                Token::String(str)
+            }
+            ch @ _ => {
+                let mut str = String::from(ch);
+                if let ch @ '#' = ch{
+                    match code.eat() {
+                        Some(' ' | '(' | ')' | '\'' | '`') => return Token::Sign(ch),
+                        Some(ch @ _) => str.push(ch),
+                        _ => return Token::Sign(ch)
+                    }
+                }
+                while let Some(c) = code.eat() {
+                    match c {
+                        ' ' | ')' | '(' => break,
+                        _ => str.push(c)
+                    }
+                }
+                match as_number_lisp(&str) {
+                    Some(x) => Token::Number(x),
+                    None => Token::Atom(str)
+                }
+            }
+        }
+        None => Token::EOF
+    }
+}
+
+fn as_number_char(ch: char) -> Option<f64> {
+    match ch {
+        '0'..='9' => Some(((ch as u8) - ('0' as u8)) as f64),
+        'a'..='z' => Some(((ch as u8) - ('a' as u8) + 10) as f64),
+        'A'..='Z' => Some(((ch as u8) - ('A' as u8) + 10) as f64),
+        _ => None
+    }
+}
+
+fn as_number_string(str: &str,base: u8) -> Option<f64>{
+    if base > 36 && base < 2{
+        return None;
+    }
+    let base = base as f64;
+    let mut sum = 0.0;
+    for ch in str.chars() {
+        sum *= base;
+        match as_number_char(ch) {
+            Some(x) if x < base => sum += x,
+            _ => return None
+        }
+    }
+    Some(sum)
+}
+
+fn as_number_lisp(string: &str) -> Option<f64> {
+    let mut itr = string.chars();
+    match itr.next() {
+        Some('0'..='9') => as_number_string(string, 10),
+        Some('#') =>{
+            match itr.next() {
+                Some('b' | 'B') => as_number_string(&itr.as_str(), 2),
+                Some('o' | 'O') => as_number_string(&itr.as_str(), 8),
+                Some('x' | 'X') => as_number_string(&itr.as_str(), 16),
+                Some('0'..='9') => {
+                    let mut i = 2;
+                    let str = loop{
+                        match itr.next() {
+                            Some('0'..='9') => i += 1,
+                            Some('r') => break &string[1..i],
+                            _ => return None,
+                        }
+                    };
+                    if let Some(base) = as_number_string(&str, 10){
+                        as_number_string(&itr.as_str(), base as u8)
+                    }else {
+                        None
+                    }
+                }
+                _ => None
+            }
+        }
+        _ => None
+    }
 }
 
 struct TestCode{
@@ -138,12 +147,11 @@ impl Code for TestCode{
 }
 
 fn main() {
-    let first = TestCode::new(r#"(abc (1234 224(1234(1234 asf qfq)("12(3      4)1") "1432" )) )"#);
-    let mut lexer = Lexer::new(first);
+    let mut first = TestCode::new(r#"1234 #b1101 #o770 #xafed #36rabcdefghijklmnopqrstuvwxyz #0002r 123412"#);
     loop{
-        match lexer.next() {
-            Snippet::EOF => break,
-            snippet @ _ => println!("{:?}",snippet.token())
+        match lexer(&mut first) {
+            Token::EOF => break,
+            token @ _ => println!("{:?}",token)
         }
     }
 }
